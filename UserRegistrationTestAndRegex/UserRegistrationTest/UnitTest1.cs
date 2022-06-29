8585420d4e76ffb5c688b2baae17515da188a828
shreegowri <gaganandh@gmail.com>
2022-06-29 09:00:55 +0530

    [shreegowri]user need to enter valid First name

---
+++ UserRegistrationTestAndRegex/UserRegistrationTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+using UserRegistrationTestAndRegex;
+
+namespace UserRegistrationTest
+{
+    public class Tests
+    {
+        UserRegistrationDetails user;
+        [SetUp]
+        public void Setup()
+        {
+            user = new UserRegistrationDetails();
+        }
+
+        [Test]
+        public void FirstName()
+        {
+            // Arrange 
+            bool expected = true;
+            string name = "Shivaraj";
+            UserRegistrationDetails user = new UserRegistrationDetails();
+
+            // Act
+            bool result = user.ValidateFirstName(name);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
